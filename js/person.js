5dfe254d4e8424655e754309f845cf44cf2cd21b
Jen Li <wanjun.s.li@gmail.com>
2014-12-03 02:39:36 -0500

    Added functionality for campaign finance. Some issues occur  b/c some candidates will throw 404 or is not found in the proper year. Also, need to add to index.html, currently only logging the data.

--- js/person.js
+++ js/person.js
@@ -1,20 +1,252 @@
 // API keys
 var auth = {
   version: "v3",
-  article_search_api_key: "7089b4dbf0dbfe27a732e031b0d1c4db:2:70134955",
-  campaign_finance_api_key: "a0010dc488c36308d3fef83ccfa40e0e:11:70134955",
-  congress_api_key: "8572e441d94d60fe07cab9d6baad0af8:16:70134955",
-  times_newswire_api_key: "2aec92c5eb73d8c60d8b594c39a4867e:1:70134955",
+  article_search_api_key: "",
+  campaign_finance_api_key: "",
+  congress_api_key: "",
+  times_newswire_api_key: "",
 }
 
 var members = [];
 
 $(document).ready(function() {
   getMemberBio();
+
 });
 
 $(document).keydown(function(key) {
+  if (key.altKey) {
+    if ($("body").hasClass("modal-open")) {
+      switch (key.keyCode) {
+        case 83:
+          if (!$("#btnSearch").attr("disabled"))
+            $("#btnSearch").get(0).click();        
+          break;
+        case 67:
+          $("#btnClose").trigger("click");
+          $("#btnHelpClose").trigger("click");
+          break;
+      }
+    }
+    else {
+      switch(key.keyCode) {    
+        case 83:
+          $("#btnSearchModal").trigger("click");          
+          break;
+        case 70:
+          $("#dboFavorite").trigger("click");
+          key.preventDefault();
+          break;
+        case 72:
+          $("#btnHelpModal").get(0).click();
+          break;
+      }
+    }
+  }
+  else {
+    switch(key.keyCode) {    
+      case 13:
+        if ($("body").hasClass("modal-open")) {
+          $("#btnSearch").get(0).click();
+          key.preventDefault();
+        }
+        break;
+        
+      case 27:
+        if ($("body").hasClass("modal-open")) {
+          $("#btnClose").trigger("click");
+          $("#btnHelpClose").trigger("click");
+          key.preventDefault();
+        }
+        break;
+    }
+  }
 });
+
+// convert integer value into $xxx,xxx,xxx.xx format
+function intToDollar(num) {
+    return "$" + num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+// get campaign finance details by member 
+function getCampaignFinance(member) {
+  fec_id = ""
+
+  name = member.name 
+  first_name = name.split(" ")[0].toUpperCase()
+  last_name = name.split(" ")[1].toUpperCase()
+
+  // to calculate the year that the member was elected, find the next election year possible and subtract 6 for Senate
+  // if House, leave as 2014, all House members were elected/re-elected in 2014
+  year = member.next_election
+
+  // set district for senate to 1, NYT API has bug that requires district for some Senate members 
+  if (member.chamber == "senate") 
+  {
+    member.district = 1
+    year = year - 6
+  }
+  else
+  {
+    year = 2014
+  }
+  $.ajax
+  ({
+    url: "http://api.nytimes.com/svc/elections/us/v3/finances/" + year + "/seats/" + member.state + "/" + member.chamber + "/" + member.district + ".json?api-key=" + auth.campaign_finance_api_key,
+    type: "GET", 
+    dataType: "jsonp", 
+    cache: true, 
+    success: function(data)
+    {
+      $.each(data["results"], function(i, result) 
+      {
+        candidate_name = result['candidate']['name'].split(", ")
+        if (candidate_name.length > 1)
+        {
+          candidate_last_name = candidate_name[0].toUpperCase()
+          candidate_first_name = candidate_name[1].split(" ")[0].toUpperCase()
+        }
+        else
+        {
+          candidate_last_name = candidate_name[0].toUpperCase()
+          candidate_first_name = ""
+        }
+
+        if (candidate_last_name == last_name && candidate_first_name == first_name)
+        {
+          fec_id = result['candidate']['id']
+          $.ajax
+          ({
+            url: "http://api.nytimes.com/svc/elections/us/v3/finances/" + year + "/candidates/" + fec_id + ".json?api-key=" + auth.campaign_finance_api_key, 
+            type: "GET", 
+            dataType: "jsonp",
+            cache: true, 
+            success: function(data)
+            {
+              response = data['results'][0]
+              member.financial_campaign_cycle = year
+              member.fec_url = response['fec_uri']
+              member.total_contributions = intToDollar(response['total_contributions'])
+              member.total_disbursements = intToDollar(response['total_disbursements'])
+              member.total_from_individuals = intToDollar(response['total_from_individuals'])
+              member.total_from_pacs = intToDollar(response['total_from_pacs'])
+              member.total_receipts = intToDollar(response['total_receipts'])
+              member.total_refunds = intToDollar(response['total_refunds'])
+            },
+            error: function(error){
+              console.log(error)
+            }
+          })
+        }
+      })
+    }, 
+    error: function(error)
+    {
+      console.log(error)
+    }
+  })
+}
+
+
+//Article display code --Connor
+function displayArticles(){
+  
+  $('#articleSection').empty();
+
+  var url1;
+  var url2;
+  var articleArray1;
+  var articleArray2;
+  var senatePage;
+  var alternate;
+  var memberName = members[0].name;
+  var pos = memberName.search(" ");
+  var fName = memberName.slice(0, pos);
+  var lName = memberName.slice(pos + 1, memberName.length);
+  
+  url1 = "http://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + fName + "+" + lName + "&sort=newest&api-key=" + auth.article_search_api_key;
+  if(members.length > 1)
+  {
+      memberName = members[1].name;
+      pos = memberName.search(" ");
+      fName = memberName.slice(0, pos);
+      lName = memberName.slice(pos + 1, memberName.length);
+      
+      url2 = "http://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + fName + "+" + lName + "&sort=newest&api-key=" + auth.article_search_api_key;
+      senatePage = true;
+  }
+  
+  //first article search, done whether there are two members on page or not.
+  $.ajax({
+      url: url1,
+      type: "get",
+      dataType: "json",
+      cache: true,
+      success:function(json){
+
+          json;
+          articleArray1 = json.response.docs;
+          
+          //If the page has two members, then only 3 articles for the first member will be displayed in this ajax call
+          if(senatePage)
+          {
+              for(var i = 0; i < 3; i++)
+              {
+                  $('#articleSection').append("<div class=\"well\">" + "<h2><a href=\"" + articleArray1[i].web_url + "\" target=\"_blank\">" 
+                                              + articleArray1[i].headline.main + "</a><h2><p class=\"articleDetail\">"
+                                              + articleArray1[i].byline.original + "<br>" 
+                                              + articleArray1[i].pub_date + "<br></p><p class=\"snippet\">" 
+                                              + articleArray1[i].snippet + "</p></div>");
+              }   
+          }
+          else
+          {
+              for(var i = 0; i < 5; i++)
+              { 
+                  $('#articleSection').append("<div class=\"well\">" + "<h2><a href=\"" + articleArray1[i].web_url + "\" target=\"_blank\">" 
+                                              + articleArray1[i].headline.main + "</a><h2><p class=\"articleDetail\">"
+                                              + articleArray1[i].byline.original + "<br>" 
+                                              + articleArray1[i].pub_date + "<br></p><p class=\"snippet\">" 
+                                              + articleArray1[i].snippet + "</p></div>");
+              }
+          }
+      },
+      error:function(){
+          alert("Error")
+      },
+  });
+
+  //If it is a senate page then another search for articles will be done for the other senator on the page
+  //The articles are appended to the bottom of the list of articles on screen. This functionality can be
+  //changed, but I did it this way because of the asynchronous behavior of the ajax calls
+  if(senatePage)
+  {
+      $.ajax({
+          url: url2,
+          type: "get",
+          dataType: "json",
+          cache: true,
+          success:function(json){
+
+              json;
+              articleArray2 = json.response.docs;
+   
+              for(var i = 0; i < 3; i++)
+              {
+                  $('#articleSection').append("<div class=\"well\">" + "<h2><a href=\"" + articleArray2[i].web_url + "\" target=\"_blank\">" 
+                                              + articleArray2[i].headline.main + "</a><h2><p class=\"articleDetail\">"
+                                              + articleArray2[i].byline.original + "<br>" 
+                                              + articleArray2[i].pub_date + "<br></p><p class=\"snippet\">" 
+                                              + articleArray2[i].snippet + "</p></div>");
+              }
+          
+          },
+          error:function(){
+              alert("Error")
+          },
+      });
+  }
+}
 
 function btnShowSearchOnClick() {
   $("#divSearch").slideDown("slow");
@@ -37,18 +269,22 @@ function getMemberBio() {
 
   $.ajax({
     url: url,
-		type: "get",
-		dataType: "json",
-		cache: true,
+    type: "get",
+    dataType: "json",
+    cache: true,
     success: function(data) {
       $.each(data["results"], function(i, result) {
         member = {
           id: result["id"],
           name: result["name"],
+          state: state, 
+          chamber: chamber, 
+          district: district, 
           role: result["role"],
           gender: result["gender"],
           party: result["party"],
           district: result["district"],
+          next_election: result["next_election"],
           expanded: false,
           loaded: false,
           favorite: false,
@@ -57,10 +293,11 @@ function getMemberBio() {
         members[i] = member;        
       });
       
+      displayArticles();
       renderMembers(-1);      
     },
     error: function() {
-      $("#divBody").empty().append("2 A problem occurred during loads. Please try again a few seconds later.");
+      $("#divBody").empty().append("A problem occurred when loading data. Please try again a few seconds later.");
     },
   });
 }
@@ -112,9 +349,12 @@ function renderMembers(index) {
     if (members[i].district != undefined && members[i].district.length > 0)
       content = content + "<label>District:&nbsp;&nbsp;&nbsp;</label><text>" + members[i].district + "</text><br/>";
     
-    if (members[i].most_recent_vote != undefined && members[i].most_recent_vote.length > 0)
-      content = content + "<label>Most Recent Vote:&nbsp;&nbsp;&nbsp;</label><text>" + formatDate(members[i].most_recent_vote) + "</text><br/>";
+    //if (members[i].most_recent_vote != undefined && members[i].most_recent_vote.length > 0)
+    //  content = content + "<label>Most Recent Vote:&nbsp;&nbsp;&nbsp;</label><text>" + formatDate(members[i].most_recent_vote) + "</text><br/>";
     
+    if (members[i].next_election != undefined && members[i].next_election.length > 0)
+      content = content + "<label>Next Election Year:&nbsp;&nbsp;&nbsp;</label><text>" + members[i].next_election + "</text><br/>";
+
     content = content + "<br/>";
     
     if (members[i].url != undefined && members[i].url.length > 0)
@@ -163,13 +403,16 @@ function btnMemberOnClick(i) {
         else
           members[i].expanded = true;
           
+        getCampaignFinance(members[i]); 
         renderMembers(i);
-        
+        // uncomment if you want to see the campaign finance details, will fix so it shows under the person's bio
+        //console.log(members[i])
+
         $("#spnMember" + i).attr("class", 'glyphicon glyphicon-collapse-down');
         $("#divMemberDetail" + i).slideDown("slow");
       },   
       error: function() {
-        $("#divBody").empty().append("1 A problem occurred during loads. Please try again a few seconds later.");
+        $("#divBody").empty().append("A problem occurred when loading data. Please try again a few seconds later.");
       },
     });
   }
